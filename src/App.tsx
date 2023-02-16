e9a6749d6b656c828d8b0a0f4f50b39974182598
Will-Andrade <willian.santos.andra@outlook.com>
2023-02-15 22:15:18 -0400

    feat: finished the quote generator

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback, useEffect, useState } from 'react';
+import classes from './App.module.css';
+
+type Quote = {
+  text: string; 
+  author: string;
+};
+
+const getRandomQuote = (quotes: Quote[]) => 
+  quotes[Math.floor(Math.random() * quotes.length)];
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [quotes, setQuotes] = useState<Quote[]>([]);
+  const [quote, setQuote] = useState<Quote | null>(null);
+
+  useEffect(() => {
+    const getQuotes = async () => {
+      try {
+        const response = await fetch ('https://type.fit/api/quotes');
+
+        if (!response.ok) throw new Error('Error fetching data!');
+
+        const responseData = await response.json();
+        
+        setQuotes(responseData.slice(0, 100));
+      } catch(err) {
+        console.log(err);
+      }
+    };
+    
+    getQuotes();
+    setQuote(quotes[0]);
+  }, []);
+
+
+  const setNewQuoteHandler = () => {
+    setQuote(getRandomQuote(quotes));
+  }
+
+  return (<main>
+    <h1>React Quote Generator</h1>
+    <article>
+      <button onClick={setNewQuoteHandler}>New Quote</button>
+      <q>{quote?.text}</q>
+      <p>- {quote?.author === null ? 'Author Unknown' : quote?.author}</p>
+    </article>
+  </main>);
 }
 
 export default App;
